9114ecf46e1c5d6e20d430bb6435dc5188fd1ca9
Festive121 <79292822+Festive121@users.noreply.github.com>
2023-02-22 04:39:13 +0000

    i dont really know what this does

---
+++ test2.py
@@ -0,0 +1,154 @@
+# uvicorn main:app --host 192.168.1.105 --port 8000
+# cloudflared tunnel --config /home/shack/.cloudflared/config.yaml run
+
+import RPi.GPIO as GPIO
+import time
+import subprocess
+import secrets
+import asyncio
+from fastapi import FastAPI, Depends, Request, Response, HTTPException, status
+from fastapi.responses import PlainTextResponse
+from fastapi.routing import APIRoute
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi_sessions.backends.implementations import InMemoryBackend
+from fastapi_sessions.session_verifier import SessionVerifier
+from fastapi_sessions.frontends.implementations import SessionCookie, CookieParameters
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from starlette.responses import HTMLResponse
+from typing import Callable
+from uuid import UUID, uuid4
+
+
+class SessionData(BaseModel):
+    username: str
+
+class CustomRoute(APIRoute):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.lock = asyncio.Lock()
+
+    def get_route_handler(self) -> Callable:
+        original_route_handler = super().get_route_handler()
+
+        async def custom_route_handler(request: Request) -> Response:
+            await self.lock.acquire()
+            response: Response = await original_route_handler(request)
+            self.lock.release()
+            return response
+
+        return custom_route_handler
+
+app = FastAPI()
+security = HTTPBasic()
+auth = False
+cookies = CookieParameters()
+cookie = SessionCookie(
+    cookie_name="cookie",
+    identifier="general_verifier",
+    auto_error=True,
+    secret_key="DONOTUSE"
+    cookie_params=cookie_params,
+)
+backend = InMemoryBackend[UUID, SessionData]()
+
+class BasicVerifier(SessionVerifier[UUID, SessionData]):
+    def __init__(
+        self,
+        *,
+        identifier: str,
+        auto_error: bool,
+        backend: InMemoryBackend[UUID, SessionData],
+        auth_http_exception: HTTPException,
+    ):
+        self._identifier = identifier
+        self._auto_error = auto_error
+        self._backend = backend
+        self._auth_http_exception = auth_http_exception
+    
+    @property
+    def identifier(self:
+        return self._identifier)
+
+    @property
+    def backend(self):
+        return self._backend
+
+    @property
+    def auto_error(self):
+        return self._auto_error
+    
+    @property
+    def auth_http_exception(self):
+        return self._auth_http_exception
+    
+    def verify_session(self, model: SessionData) -> bool;
+        return True
+
+verifier = BasicVerifier(
+    identifier="general_verifier",
+    auto_error=True,
+    backend=backend,
+    auth_http_exception=HTTPException(status_code=403, detail="invalid session"),
+)
+
+app.openapi = {"info": {"title": "Remote Shock", "verison": "1.0.0"}}
+app.router.route_class = CustomRoute
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    current_username_bytes = credentials.username.encode("utf8")
+    correct_username_bytes = b"jack is so cool"
+    is_correct_username = secrets.compare_digest(current_username_bytes, correct_username_bytes)
+    
+    current_password_bytes = credentials.password.encode("utf8")
+    correct_password_bytes = b"i, ian salyer agree"
+    is_correct_password = secrets.compare_digest(current_password_bytes, correct_password_bytes)
+    
+    if not (is_correct_username and is_correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+
+@app.get("/")
+async def read_current_user(response: Response, str = Depends(get_current_username)):
+    global auth
+    auth = True
+    
+    session = uuid4()
+    data = SessionData(username=USER)
+
+    await backend.create(session, data)
+    cookie.attach_to_response(response, session)
+
+    response.headers["Location"] = "/home"
+    response.status_code = 302
+
+@app.get("/home", response_class=HTMLResponse, dependencies=[Depends(cookie)])
+async def read_item(request: Request, session_data: SessionData = Depends(verifier)):
+    if auth:
+        return templates.TemplateResponse("index.html", {"request": request})
+    else:
+        return PlainTextResponse(content="UNAUTHORIZED")
+
+@app.get("/run")
+def runit(response: Response):
+    if auth:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(18, GPIO.OUT)
+        GPIO.output(18, True)
+        time.sleep(1)
+        GPIO.output(18, False)
+        response.headers["Location"] = "/"
+        response.status_code = 302
+    else:
+        return PlainTextResponse(content="UNAUTHORIZED")
+
