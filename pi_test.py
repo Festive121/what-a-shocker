afc36ba69ca9a0c4a6ed3c3495a8a97676dc1162
Festive121 <79292822+Festive121@users.noreply.github.com>
2023-02-23 01:12:13 +0000

    drui

--- pi_test.py
+++ pi_test.py
@@ -9,9 +9,6 @@ app = FastAPI()
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-while True:
-    if GPIO.input(10) == GPIO.HIGH:
         
 @app.get("/quit")
 def iquit():
