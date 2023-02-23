a880910e5cd4c497d9172af674a626fff911fe83
Festive121 <79292822+Festive121@users.noreply.github.com>
2023-02-23 01:06:45 +0000

    remove indent

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import fastapi
 import uvicorn
 import psutil
 
-    @app.get("/quit")
-    def iquit():
-        parent_pid = os.getpid()
-        parent = psutil.Process(parent_pid)
-        for child in parent.children(recursive=True):  # or parent.children() for recursive=False
-            child.kill()
-        parent.kill()
+@app.get("/quit")
+def iquit():
+    parent_pid = os.getpid()
+    parent = psutil.Process(parent_pid)
+    for child in parent.children(recursive=True):  # or parent.children() for recursive=False
+        child.kill()
+    parent.kill()
     
 if __name__ == '__main__':
     uvicorn.run(app, host='192.168.1.105', port=8000)
