59e0d9b58cfdd8d26c77fef2c80c86b65d4d74c4
Festive121 <79292822+Festive121@users.noreply.github.com>
2023-02-26 00:41:32 +0000

    chat gpt??

--- pi_test.py
+++ pi_test.py
@@ -1,24 +1,52 @@
-import os
-from fastapi import FastAPI
-import uvicorn
-import psutil
 import RPi.GPIO as GPIO
+from fastapi import FastAPI, Request, BackgroundTasks
+from fastapi.responses import HTMLResponse
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.IN)
 
 app = FastAPI()
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        
-@app.get("/quit")
-def iquit():
-    while True:
-        if GPIO.input(10) == GPIO.HIGH:
-            parent_pid = os.getpid()
-            parent = psutil.Process(parent_pid)
-            for child in parent.children(recursive=True):
-                child.kill()
-            parent.kill()
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request, background_tasks: BackgroundTasks):
+    # Define the HTML page to return
+    html_content = """
+        <html>
+            <head>
+                <title>Pin 10 Monitor</title>
+            </head>
+            <body>
+                <h1>Pin 10 Monitor</h1>
+                <p>Status: <span id="status">Inactive</span></p>
+            </body>
+            <script>
+                var statusElement = document.getElementById("status");
+                var eventSource = new EventSource("/monitor");
+                eventSource.onmessage = function(event) {
+                    statusElement.textContent = event.data;
+                };
+            </script>
+        </html>
+    """
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='192.168.1.105', port=8000)
+    async def monitor_pin():
+        while True:
+            if GPIO.input(10):
+                yield "Active"
+            else:
+                yield "Inactive"
+
+    # Start the monitor_pin function as a background task
+    background_tasks.add_task(monitor_pin)
+
+    # Return the HTML page
+    return html_content
+
+@app.get("/monitor")
+async def monitor():
+    async def pin_stream():
+        while True:
+            yield "data: {}\n\n".format(GPIO.input(10))
+            await asyncio.sleep(1)
+
+    return StreamingResponse(pin_stream(), media_type="text/event-stream")
