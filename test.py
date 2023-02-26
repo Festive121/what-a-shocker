1affb93dbcdb77a63f970827754f51cfc452e91a
Festive121 <79292822+Festive121@users.noreply.github.com>
2023-02-26 01:10:52 +0000

    tst

--- test.py
+++ test.py
@@ -1,17 +1,6 @@
 import os
 from fastapi import FastAPI
-import uvicorn
-import psutil
 
-app = FastAPI()
-
-@app.get("/quit")
-def iquit():
-    parent_pid = os.getpid()
-    parent = psutil.Process(parent_pid)
-    for child in parent.children(recursive=True):  # or parent.children() for recursive=False
-        child.kill()
-    parent.kill()
-    
-if __name__ == '__main__':
-    uvicorn.run(app, host='192.168.1.105', port=8000)
+@app.get("/shutdown")
+def shutdown():
+    os.kill(os.getpid(), 9)
