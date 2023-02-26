3d42d2273d308c88299819c61d3da0ea213d4bbc
Festive121 <79292822+Festive121@users.noreply.github.com>
2023-02-26 00:31:49 +0000

    test

--- pi_test.py
+++ pi_test.py
@@ -11,14 +11,13 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         
 @app.get("/quit")
-def iquit():
-    while True:
-        if GPIO.input(10) == GPIO.HIGH:
-            parent_pid = os.getpid()
-            parent = psutil.Process(parent_pid)
-            for child in parent.children(recursive=True):
-                child.kill()
-            parent.kill()
+async def iquit():
+    if GPIO.input(10) == GPIO.HIGH:
+        parent_pid = os.getpid()
+        parent = psutil.Process(parent_pid)
+        for child in parent.children(recursive=True):
+            child.kill()
+        parent.kill()
 
 if __name__ == '__main__':
     uvicorn.run(app, host='192.168.1.105', port=8000)
