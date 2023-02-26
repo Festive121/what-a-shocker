092332ac8fce9464a6a55b9c10df59d3ab05d498
Festive121 <79292822+Festive121@users.noreply.github.com>
2023-02-26 00:52:23 +0000

    gpt 3

--- pi_test.py
+++ pi_test.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 from fastapi import FastAPI, Request, BackgroundTasks
-from fastapi.responses import HTMLResponse, StreamingResponse
-import uvicorn
+from fastapi.responses import HTMLResponse
 import asyncio
+import uvicorn
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN)
@@ -31,20 +31,42 @@ async def read_root(request: Request, background_tasks: BackgroundTasks):
         </html>
     """
 
-    async def monitor_pin(response):
-        current_status = GPIO.input(10)
+    async def monitor_pin():
         while True:
-            if GPIO.input(10) != current_status:
-                current_status = GPIO.input(10)
-                await response.write("data: {}\n\n".format(current_status))
+            if GPIO.input(10):
+                status = "Active"
+            else:
+                status = "Inactive"
             await asyncio.sleep(1)
+            yield status
 
     # Start the monitor_pin function as a background task
-    response = StreamingResponse(monitor_pin, media_type="text/event-stream")
-    background_tasks.add_task(monitor_pin, response)
+    background_tasks.add_task(monitor_pin)
 
     # Return the HTML page
     return html_content
+
+@app.get("/monitor")
+async def monitor(background_tasks: BackgroundTasks):
+    async def pin_stream(response):
+        async for status in monitor_pin():
+            await response.write("data: {}\n\n".format(status))
+
+    async def monitor_pin():
+        while True:
+            if GPIO.input(10):
+                status = "Active"
+            else:
+                status = "Inactive"
+            await asyncio.sleep(1)
+            yield status
+
+    # Start the pin_stream function as a background task
+    response = StreamingResponse(pin_stream, media_type="text/event-stream")
+    background_tasks.add_task(response.__call__, response)
+
+    # Return the StreamingResponse object
+    return response
 
 if __name__ == '__main__':
     uvicorn.run(app, host='192.168.1.105', port=8000)
